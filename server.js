321892fd9394d70cacc7f285e57ca56c85aec8bb
unknown <shannoncoleman16@gmail.com>
2023-01-15 16:50:21 -0500

    added breaks

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const prompt = inquirer.createPromptModule();
 require('console.table');
+
 const db = mysql.createConnection({
   user: "root",
-  database: "employeelist_db"
+  database: 'employeelist_db'
 });
 
 const listOption = (type) => {
@@ -12,17 +13,23 @@ switch (type) {
   case 'VIEW ALL DEPARTMENTS': {
     db.query('SELECT * FROM department', (err, departments) => {
       console.table(departments);
+      init();
     });
+    break;
   }
   case 'VIEW ALL EMPLOYEES': {
     db.query('SELECT * FROM employee', (err, employees) => {
       console.table(employees);
+      init();
     });
+    break;
   }
   case 'VIEW ALL ROLES': {
     db.query('SELECT * FROM role', (err, roles) => {
       console.table(roles);
+      init();
     });
+    break;
   }
   }
 };
